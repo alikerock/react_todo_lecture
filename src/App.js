ed93667d10a320ddc86680d479f3550d0e007230
rock's ezweb <alikerock@gmail.com>
2023-10-27 11:23:44 +0900

    checkbox style

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Todo from './Todo';
 
 function App() {
 
+  const [todoid, setTodoid] = useState(2);
+
   const [todo,setTodo] = useState([
     {id:1, text:'learn web', checked:false},
     {id:2, text:'get a job', checked:false}
@@ -15,14 +17,27 @@ function App() {
     <Todo data={item} key={item.id} />
   ));
 
+  let addTodo = (value)=>{
+    let newTodos = [...todo];
+    let newId = todoid + 1;
+    setTodoid(newId);
+    newTodos.push({id:newId, text:value, checked:false});
+    setTodo(newTodos);
+    document.getElementById('todo').value='';
+  }
+
   return (
     <div className="container">
       <h1>Todo list</h1>
-      <Form>
-        <Form.Group className="mb-3" controlId="todo1">
+      <Form onSubmit={e=>{
+        e.preventDefault();
+        // console.log(e.target.todo.value);
+        addTodo(e.target.todo.value);
+      }}>
+        <Form.Group className="mb-3" controlId="todo">
           <Form.Label>Todo Input</Form.Label>
-          <Form.Control type="text" placeholder="할일을 입력하세요" />
-        </Form.Group>     
+          <Form.Control type="text" name="todo" placeholder="할일을 입력하세요" />
+        </Form.Group>          
       </Form>   
       <hr/>
       <div>
--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 
+
 const Todo = ({data})=>{
+  const[ischecked, setIschecked] = useState(false);
+
+  let className = 'form-check-label';
+  let deco = {};
+  if(ischecked){
+    className += ' text-muted';
+    deco = {textDecoration:'line-through'}
+  }
+
+  const handleCheckboxClick = (e)=>{        
+    setIschecked(!ischecked);
+    console.log(ischecked);
+  }
+
   return(
-    <div>
-      <Form.Check 
-        type="checkbox"
-        id={`todo-${data.id}`}
-        label={data.text}
-      />
+    <div className="form-check">
+      <input className="form-check-input " type="checkbox" value="" id={`todo${data.id}`} onChange={handleCheckboxClick} />
+      <label className={className} style={deco} htmlFor={`todo${data.id}`}>
+        {data.text}
+      </label>
     </div>
   )
 }
